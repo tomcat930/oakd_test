eefc5367757010d9edc1aadc74438911ba2e40db
Tomohiro Murayama <silhouette.formula1@gmail.com>
2024-06-23 20:36:28 +0900

    Update threading

--- spatial_yolo.py
+++ spatial_yolo.py
@@ -1,11 +1,68 @@
 #!/usr/bin/env python3
 # coding: utf-8
 import argparse
+import threading
 import cv2
 from lib.oakd_spatial_yolo import OakdSpatialYolo
 
+detections = []
+labels = []
+
+
+def test():
+    while True:
+        if detections:
+            for detection in detections:
+                print("=============================")
+                print(labels[detection.label])
+                print(detection.spatialCoordinates.x)
+                print(detection.spatialCoordinates.y)
+                print(detection.spatialCoordinates.z)
+
+
+def detect() -> None:
+    global detections
+    global labels
+    end = False
+    while not end:
+        oakd_spatial_yolo = OakdSpatialYolo(
+            config_path=args.config,
+            model_path=args.model,
+            fps=args.fps,
+            cam_debug=args.display_camera,
+            robot_coordinate=args.robot_coordinate,
+        )
+        labels = oakd_spatial_yolo.get_labels()
+        while True:
+            frame = None
+            detections = []
+            try:
+                frame, detections = oakd_spatial_yolo.get_frame()
+            except BaseException:
+                print("===================")
+                print("get_frame() error! Reboot OAK-D.")
+                print("If reboot occur frequently, Bandwidth may be too much.")
+                print("Please lower FPS.")
+                print("===================")
+                break
+            if frame is not None:
+                oakd_spatial_yolo.display_frame("nn", frame, detections)
+            if cv2.waitKey(1) == ord("q"):
+                end = True
+                break
+        oakd_spatial_yolo.close()
+
 
 def main() -> None:
+    t1 = threading.Thread(target=detect)  # 認識用スレッド
+    t2 = threading.Thread(target=test)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+
+if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -43,34 +100,4 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    end = False
-    while not end:
-        oakd_spatial_yolo = OakdSpatialYolo(
-            config_path=args.config,
-            model_path=args.model,
-            fps=args.fps,
-            cam_debug=args.display_camera,
-            robot_coordinate=args.robot_coordinate,
-        )
-        while True:
-            frame = None
-            detections = []
-            try:
-                frame, detections = oakd_spatial_yolo.get_frame()
-            except BaseException:
-                print("===================")
-                print("get_frame() error! Reboot OAK-D.")
-                print("If reboot occur frequently, Bandwidth may be too much.")
-                print("Please lower FPS.")
-                print("==================")
-                break
-            if frame is not None:
-                oakd_spatial_yolo.display_frame("nn", frame, detections)
-            if cv2.waitKey(1) == ord("q"):
-                end = True
-                break
-        oakd_spatial_yolo.close()
-
-
-if __name__ == "__main__":
     main()
